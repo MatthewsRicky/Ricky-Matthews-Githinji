eda9ca994a5f252dd0548621e9c0318b25b47f93
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-05 11:41:43 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${inter.className} bg-slate-50 text-slate-950`}>
-				<div className="bg-[#fbe2e3] absolute"></div>
+				<div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.35rem] rounded-full blur-[10rem]"></div>
 				<div></div>
 
 				{children}
