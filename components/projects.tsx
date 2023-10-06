47551cc031fcd5f665aab9f3086b79b507cb585c
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-06 22:11:55 +0300

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,7 +5,10 @@ import Project from "./project";
 
 export default function Projects() {
 	return (
-		<section>
+		<section
+			id="projects"
+			className="scroll-mt-28"
+		>
 			<SectionHeading>Projects</SectionHeading>
 			<div className="">
 				{projectsData.map((project, index) => (
