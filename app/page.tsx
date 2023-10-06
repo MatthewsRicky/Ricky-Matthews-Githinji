f0387f3b6340af4ef068997b64a9bec433e88fb1
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-06 11:36:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
 	return (
-		<main className="">
+		<main className="flex items-center flex-col px-4">
 			<Intro />
+			<SectionDivider />
 		</main>
 	);
 }
