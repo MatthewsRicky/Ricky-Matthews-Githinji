75b88b7e66180ad105bc7da7c782a9eccf2169d6
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-08 13:44:25 +0300

    Update active-section-context.tsx

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,10 +1,7 @@
 "use client";
 
+import type { SectionName } from "@/lib/types";
 import React, { createContext, useContext, useState } from "react";
-
-import { links } from "@/lib/data";
-
-type SectionName = (typeof links)[number]["name"];
 
 type ActiveSectionContextProviderProps = {
 	children: React.ReactNode;
@@ -13,6 +10,8 @@ type ActiveSectionContextProviderProps = {
 type ActiveSectionContextType = {
 	activeSection: SectionName;
 	setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+	timeOfLastClick: number;
+	setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export const ActiveSectionContext =
@@ -22,12 +21,15 @@ export default function ActiveSectionContextProvider({
 	children,
 }: ActiveSectionContextProviderProps) {
 	const [activeSection, setActiveSection] = useState<SectionName>("Home");
+	const [timeOfLastClick, setTimeOfLastClick] = useState(0); // We need to keep track of the last click to disable the observer temporarily when user clicks on a link
 
 	return (
 		<ActiveSectionContext.Provider
 			value={{
 				activeSection,
 				setActiveSection,
+				timeOfLastClick,
+				setTimeOfLastClick,
 			}}
 		>
 			{children}
