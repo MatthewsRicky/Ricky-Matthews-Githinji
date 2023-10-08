13c627ba54da50539c362d4983438dfcb46e893e
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-08 13:47:42 +0300

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
-import { useInView } from "react-intersection-observer";
-import { useActiveSectionContext } from "@/context/active-section-context";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
