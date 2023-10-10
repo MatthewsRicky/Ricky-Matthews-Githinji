30bbb9729efce38404c3a03c56538e588f085875
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-10 12:07:33 +0300

    Create experience.tsx

---
+++ components/experience.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import SectionHeading from "./section-heading";
+
+export default function Experience() {
+	return <SectionHeading>My Experience</SectionHeading>;
+}
