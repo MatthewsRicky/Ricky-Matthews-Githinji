419cc3f172d53b7364376b19ee342f0394a7c075
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-10 14:53:20 +0300

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,11 +1,42 @@
+"use client";
+
 import React from "react";
 import {
 	VerticalTimeline,
 	VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
 import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
-	return <SectionHeading>My Experience</SectionHeading>;
+	const { ref } = useSectionInView("Experience", 0.5);
+	return (
+		<section
+			ref={ref}
+			id="experience"
+		>
+			<SectionHeading>My Experience</SectionHeading>;
+			<VerticalTimeline>
+				{experiencesData.map((item, index) => (
+					<React.Fragment key={index}>
+						<VerticalTimelineElement
+							contentStyle={{
+								background: "#f3f4f6",
+								boxShadow: "none",
+								border: "1px solid rbga(0, 0, 0, 0.05)",
+								textAlign: "left",
+								padding: "1.3rem 2rem",
+							}}
+						>
+							<h3>{item.title}</h3>
+							<p>{item.location}</p>
+							<p>{item.description}</p>
+						</VerticalTimelineElement>
+					</React.Fragment>
+				))}
+			</VerticalTimeline>
+		</section>
+	);
 }
