dd688bc54fa33cbb841a0daecf933db107197c41
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-15 16:18:36 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 			className="!scroll-smooth"
 		>
 			<body
-				className={`${inter.className} bg-slate-50 text-slate-950 relative h-[5000px] pt-28 sm:pt-32`}
+				className={`${inter.className} bg-slate-50 text-slate-950 relative pt-28 sm:pt-32`}
 			>
 				<div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.35rem] sm:w-[65.75rem] rounded-full blur-[10rem]"></div>
 				<div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] sm:w-[65.75rem] rounded-full blur-[10rem] md:left[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
