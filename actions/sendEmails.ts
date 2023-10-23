f4d094f176e11e26c3c24d1d6cf76a656a521617
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-23 12:32:18 +0300

    Update sendEmails.ts

--- actions/sendEmails.ts
+++ actions/sendEmails.ts
@@ -1,7 +1,26 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const validateString = (value: unknown) => {
+	if (!value || typeof value !== "string") {
+		return;
+		false;
+	}
+	return true;
+};
+
 export const sendEmail = async (formData: FormData) => {
-	console.log("running on server");
-	console.log(formData.get("senderEmail"));
-	console.log(formData.get("senderMessage"));
+	const senderEmail = formData.get("senderEmail");
+	const message = formData.get("senderMessage");
+
+	resend.emails.send({
+		from: "onboarding@resend.dev",
+		to: "matthewsrickypro@gmail.com",
+		subject: "Message from contact form",
+		reply_to: sendEmail,
+		text: message,
+	});
 };
