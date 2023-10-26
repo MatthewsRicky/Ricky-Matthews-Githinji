fa62334283db8b86bd303847fc3eb7672bb3bd09
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-27 00:38:10 +0300

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -39,11 +39,11 @@ export default function About() {
 				developer.
 			</p>
 			<p>
-				<span className="italic">When I&apos;m not coding</span>, I enjoy
+				<span className="italic">When I`&apos;`m not coding</span>, I enjoy
 				playing video games, watching movies, and playing with my dog. I also
 				enjoy <span className="font-medium">learning new things</span>. I am
 				currently learning about{" "}
-				<span className="font-medium">history and philosophy</span>. I&apos;m
+				<span className="font-medium">history and philosophy</span>. I`&apos;`m
 				also learning how to play the guitar.
 			</p>
 			<motion.div
