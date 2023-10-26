de78d63007a3d3b971a179a633951e831e19537d
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-26 22:07:42 +0300

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -15,12 +15,12 @@ export default function Header() {
 	return (
 		<header className="z-[999] relative">
 			<motion.div
-				className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-80 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-slate-950 dark:border-black/40"
+				className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-80 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-slate-900 dark:border-black/40 dark:bg-opacity-75"
 				initial={{ y: -100, x: "-50%", opacity: 0 }}
 				animate={{ y: 0, x: "-50%", opacity: 1 }}
 			></motion.div>
 			<nav className=" flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-				<ul className="flex w-[22rem] flex-wrap justify-center items-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+				<ul className="flex w-[22rem] flex-wrap justify-center items-center gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
 					{links.map((link) => (
 						<motion.li
 							className="h-3/4 flex items-center justify-center relative"
@@ -30,7 +30,7 @@ export default function Header() {
 						>
 							<Link
 								className={clsx(
-									"flex  w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
+									"flex  w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition dark:text-slate-400 dark:hover:text-slate-300",
 									{
 										"text-slate-950": activeSection === link.name,
 									}
@@ -51,7 +51,7 @@ export default function Header() {
 											stiffness: 380,
 											damping: 30,
 										}}
-										className="bg-slate-100 rounded-full absolute inset-0 -z-10"
+										className="bg-slate-100 dark:bg-slate-700 rounded-full absolute inset-0 -z-10"
 									></motion.span>
 								)}
 							</Link>
