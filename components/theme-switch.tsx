24f39ba0877f8bbac1b670e2ac2292ad608dc800
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-26 08:52:35 +0300

    Update theme-switch.tsx

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
+	const [theme, setTheme] = useState("light");
+
 	return (
-		<button className="fixed bottom-10 right-10 bg-whihte w-[3rem] h-[3rem] ">
+		<button
+			className="fixed bottom-10 right-10 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex justify-center items-center hover:scale-[1.15] active:scale-105 transition-all"
+			onAuxClickCapture={() => {}}
+		>
 			<BsSun />
 		</button>
 	);
