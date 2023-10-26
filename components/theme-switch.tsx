859803016e54cebded05fb4fd7ca5dbd3327b2b8
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-26 08:42:19 +0300

    Update theme-switch.tsx

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -3,7 +3,7 @@ import { BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
 	return (
-		<button>
+		<button className="fixed bottom-10 right-10 bg-whihte w-[3rem] h-[3rem] ">
 			<BsSun />
 		</button>
 	);
