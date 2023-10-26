f2f3758247e4d36f49e6811f633fad2ffa7d6918
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-10-26 09:05:02 +0300

    Update theme-switch.tsx

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { BsSun } from "react-icons/bs";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 type Theme = "light" | "dark";
 
@@ -11,15 +11,19 @@ export default function ThemeSwitch() {
 	const toggleTheme = () => {
 		if (theme === "light") {
 			setTheme("dark");
+			window.localStorage.setItem("theme", "dark");
+		} else {
+			setTheme("light");
+			window.localStorage.setItem("theme", "light");
 		}
 	};
 
 	return (
 		<button
 			className="fixed bottom-10 right-10 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex justify-center items-center hover:scale-[1.15] active:scale-105 transition-all"
-			onAuxClickCapture={toggleTheme}
+			onClick={toggleTheme}
 		>
-			<BsSun />
+			{theme === "light" ? <BsSun /> : <BsMoon />}
 		</button>
 	);
 }
